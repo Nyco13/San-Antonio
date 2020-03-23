b80157e161b0add3607750c6bdaeda9236b84c39
Nicolas <noelnicola@hotmail.com>
2020-03-23 21:03:28 +0100

    2nd Push

--- SanAntonio.py
+++ SanAntonio.py
@@ -1,13 +1,25 @@
 
 import random
-Quote = ["Tu ne l'emporteras pas au paradis", "Tant va la cruche à l'eau quà la fin elle se casse", "Les plus faibles ont toujours tort", "Qui vivra verra", "Un tien vaut mieux que 2 tu l'auras"]
-Characters = ["San Antonio", "Felicie", "Antoine", "César", "Mathias"]
+import json
 
-def MainFunction()
-    MainVar= input("Appuyez sur Entrée pour avoir une citation et un personnage de San Antonio; Appuyez sur E pour terminer le programme")
-    if MainVar == E : pass
-    else item = random.randrange(0;len(Quote))
-        print(Quote[item])
-        print(Characters[item])
+Quotes = ["Tu ne l'emporteras pas au paradis", "Tant va la cruche à l'eau qu'à la fin elle se casse", "Les plus faibles ont toujours tort", "Qui vivra verra", "Un tien vaut mieux que 2 tu l'auras"]
+Characters = ["San Antonio", "felicie", "Antoine", "césar", "Mathias"]
 
-while MainFunction() !=E :
+def message(character, quote):
+    n_character =character.capitalize()
+    n_quote = quote.capitalize()
+    return "{} a dit : {}".format(n_character, n_quote)
+
+def RandomQuotes():
+    RandomQ = random.randint(0,len(Quotes))
+    return RandomQ
+    
+def RandomChar():
+    RandomC = random.randint(0,len(Characters))
+    return RandomC
+
+MainVar= input("Appuyez sur Entrée pour avoir une citation et un personnage de San Antonio; Appuyez sur E pour terminer le programme")
+while MainVar != "E" :
+    RandomQuotes()
+    RandomChar()
+    print(message(Characters[RandomC],Quotes[RandomQ]))

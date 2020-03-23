0ba0f8c5d805cc918d919bb95110a598b5384a44
Nicolas <noelnicola@hotmail.com>
2020-03-23 19:32:28 +0100

    First additions Py

--- SanAntonio.py
+++ SanAntonio.py
@@ -1,1 +1,13 @@
-print ("Yo")
+
+import random
+Quote = ["Tu ne l'emporteras pas au paradis", "Tant va la cruche à l'eau quà la fin elle se casse", "Les plus faibles ont toujours tort", "Qui vivra verra", "Un tien vaut mieux que 2 tu l'auras"]
+Characters = ["San Antonio", "Felicie", "Antoine", "César", "Mathias"]
+
+def MainFunction()
+    MainVar= input("Appuyez sur Entrée pour avoir une citation et un personnage de San Antonio; Appuyez sur E pour terminer le programme")
+    if MainVar == E : pass
+    else item = random.randrange(0;len(Quote))
+        print(Quote[item])
+        print(Characters[item])
+
+while MainFunction() !=E :
